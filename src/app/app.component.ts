48859bb8459ae24e95dbfec7f51d5c406c762e2f
omerozoglu <omerozoglu.1@gmail.com>
2020-03-22 19:03:54 +0300

    #name and (click)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { AppModel } from "./Model/app.model";
 import { isNgTemplate } from "@angular/compiler";
+import { empty } from "rxjs";
+import { TodoItem } from "./Model/todoitem.model";
 
 @Component({
   selector: "app-root",
@@ -9,12 +11,21 @@ import { isNgTemplate } from "@angular/compiler";
 })
 export class AppComponent {
   appModel: AppModel = new AppModel();
+
   getTitle() {
     return this.appModel.title;
   }
+
   getTodoItems() {
     return this.appModel.items.filter(i => !i.action);
   }
+
+  addItem(value) {
+    if (value != "") {
+      this.appModel.items.push(new TodoItem(value, false));
+    }
+  }
+
   trackByFn(index: number, item: any): number {
     return index;
   }
